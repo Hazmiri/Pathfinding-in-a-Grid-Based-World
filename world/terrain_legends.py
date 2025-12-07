8ba6bc269dd2b08f4bfa009259ec7fe69b6b6ffc
Saladin <hazmiri-sm@hotmail.com>
2025-12-07 18:56:59 +0000

    Creat world folder, and starting the Terrain catalogue

---
+++ world/terrain_legends.py
@@ -0,0 +1,26 @@
+""" 
+terrain_legends.py
+------------------
+
+This module defines the legendary terrains of Aris's world.
+Each terrain has:
+    - A peatic name
+    - A movement cost (energy required)
+    - A passability flag
+    - A map symbol (for ASCII rendering)
+    
+"""
+from math import inf
+from typing import Dict
+
+# -----------------------------------------------------------
+# TERRAIN CATALAGUE (CREATIVE + RICH)
+#
+# I can adjust names or cost depends if my imagination grows will I am writing the code.
+# 
+# All keys are lowwercase for JSON compatibility.
+# -----------------------------------------------------------
+
+TERRAIN_CATALOGUE: Dict[str, float] = {
+    
+}
