87e023741b37cc4ff1a0e4a63853e33f12b4e1c4
Saladin <hazmiri-sm@hotmail.com>
2025-12-08 15:00:18 +0000

    Scaffold Saladin_Pathfinder

--- aris/saladin_pathfinder.py
+++ aris/saladin_pathfinder.py
@@ -1,5 +1,15 @@
 """
 saladin_pathfinder.py
 ----------------------
+This module defines the Saladin-pathfinder, an A* (A-start) based pathfinding
+engien tailored for Aris the Explorer.
+
+It supports two optimisation strategies:
+
+    1. 'fewest_steps' - minimises the number of moves.
+    2. 'lowest_energy' - minimises terrain-weighted movement cost.
+    
+The algorithm operates on a Map_Anvil instance, which provides information
+about terrain costs and neighbour accessibility.
 
 """
