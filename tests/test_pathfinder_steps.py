1f58680d99e05b2554cacf70015fa5ba8ff14770
Saladin <hazmiri-sm@hotmail.com>
2025-12-09 11:50:12 +0000

    Suite of unit tests that ensures A* algorithm, when set to the fewest_steps

---
+++ tests/test_pathfinder_steps.py
@@ -0,0 +1,9 @@
+# tests/test_pathfinder_steps.py
+"""
+Tests for Saladin_Pathfinder in 'fewest_steps' mode.
+Ensures shortest paths are computed based solely on step count.
+"""
+
+from runes.runes import PathGlyph
+from world.grid_forge import Map_Anvil
+from aris.saladin_pathfinder import Saladin_Pathfinder
